108511a8ac46d45e504f4b578f0654c22160ff5b
Rad8433 <1804433@cmontmorency.qc.ca>
2025-09-02 10:23:28 -0400

    Pixel on off avec slider

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
+#include <fastLED.h>
 #define MA_BROCHE_ANGLE 32
+CRGB monPixel;
 
 
 void setup() {
+  FastLED.addLeds<WS2812,27,GRB>( &monPixel,1);
   Serial.begin(115200);
 }
 
@@ -11,4 +14,12 @@ void loop() {
   Serial.print(maLectureAngle);
   Serial.println();
   delay(100);
+
+  if (maLectureAngle > 2047){
+    monPixel = CRGB(0,222,255);
+    FastLED.show();
+  } else{
+    monPixel = CRGB(0,0,0);
+    FastLED.show();
+  }
 }
